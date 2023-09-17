91d83a8da540c8ce0c8ba66b0e82a90313fc430e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-09-17 14:18:38 +0300

    Added some more configuration

--- terraform_intro.tf
+++ terraform_intro.tf
@@ -5,4 +5,7 @@ provider "aws" {
 module "intro_vpc" {
   source = "terraform-aws-modules/vpc/aws"
   cidr = "10.7.0.0/16"
+
+  azs = ["us-east-1a", "us-east-1c"]
+  public_subnets = ["10.7.100.0/24", "10.7.200.0/24"]
 }
